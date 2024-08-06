31a089f20a552db9a83356b89c99992d126b47f7
= <=>
2024-08-06 15:23:40 +0530

    working: with some imp* concepts in JS

---
+++ js-data-types/conversion.js
@@ -0,0 +1,15 @@
+// number to string conversion
+const num = 12;
+
+String(num); // "12"
+num.toString(); // "12"
+num + ""; // "12"
+
+// string to number conversionssss
+const str = "12";
+
+Number(str); // 12
++str; // 12
+parseInt(str); // 12
+
+// If the number is floating, parseFloat can be used. parseInt and parseFloat are the methods present on the Number object also
---
+++ js-data-types/primititves.js
@@ -0,0 +1,19 @@
+typeof 50; //   "number"
+typeof "text"; //   "string"
+typeof true; //   "boolean"
+typeof undefined; //   "undefined"
+typeof function () {}; //   "function"
+typeof 10n; //   "bigint"
+typeof Symbol(); //   "symbol"
+typeof [1, 2]; //   "object"
+typeof {}; //   "object"
+
+typeof NaN; //   "number"        (NaN is Not a Number)
+typeof undeclaredVar; //   "undefined"     (undeclaredVariable is never declared)
+typeof Infinity; //   "number"        (Infinity, -Infinity, -0 are all valid numbers in JavaScript)
+typeof null; //   "object"        (This stands since the beginning of JavaScript)
+typeof /regex/; //   "object"        (regular expressions start and end with '/' in literal form)
+
+// Notes
+
+// Arrays and functions are sub type of objects
